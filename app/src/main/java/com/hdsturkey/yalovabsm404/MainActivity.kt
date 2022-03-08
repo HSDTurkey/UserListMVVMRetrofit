dd98cd042a714cfbab1790c2fe9dab01c05dc39c
Cengiz TORU <cengiztoru@gmail.com>
2022-03-08 17:24:06 +0300

    Menu clicks handled

--- app/src/main/java/com/hdsturkey/yalovabsm404/MainActivity.kt
+++ app/src/main/java/com/hdsturkey/yalovabsm404/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.widget.Toast
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +20,30 @@ class MainActivity : AppCompatActivity() {
         val inflater = menuInflater
         inflater.inflate(R.menu.opt_menu, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    //3- Handle Menu Clicks
+    /**
+     * Return false to allow normal menu processing to proceed, true to consume it here.
+     * */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.menu_add -> {
+                toast("Add Menu Clicked")
+                return true
+            }
+            R.id.menu_edit -> {
+                toast("Edit Menu Clicked")
+                return true
+            }
+            R.id.menu_delete -> {
+                toast("Delete Menu Clicked")
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+
     }
 
     fun Context.toast(message: String, length: Int = Toast.LENGTH_SHORT) {
