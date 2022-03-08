551a36660a7493f27436b307050284fa14506385
Cengiz TORU <cengiztoru@gmail.com>
2022-03-08 17:22:10 +0300

    Options Menu Inflated

--- app/src/main/java/com/hdsturkey/yalovabsm404/MainActivity.kt
+++ app/src/main/java/com/hdsturkey/yalovabsm404/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.hdsturkey.yalovabsm404
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
+    //2- Ovveride Menu Creation Process
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.opt_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
 }
