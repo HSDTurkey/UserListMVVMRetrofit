58f01c5007071b763c87a7f24768d9431c55d4cd
Cengiz TORU <cengiztoru@gmail.com>
2022-03-15 15:57:47 +0300

    Bug fixed :)

--- app/src/main/java/com/hdsturkey/yalovabsm404/fragments/user_list/UserListFragment.kt
+++ app/src/main/java/com/hdsturkey/yalovabsm404/fragments/user_list/UserListFragment.kt
@@ -1,7 +1,9 @@
 package com.hdsturkey.yalovabsm404.fragments.user_list
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.hdsturkey.yalovabsm404.R
@@ -13,7 +15,6 @@ class UserListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mBinding = FragmentUserListBinding.inflate(layoutInflater)
 
         setListeners()
 
@@ -23,5 +24,13 @@ class UserListFragment : Fragment() {
         mBinding.tvUserList.setOnClickListener {
             findNavController().navigate(R.id.action_userListFragment_to_userDetailFragment)
         }
+    }
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        mBinding = FragmentUserListBinding.inflate(layoutInflater)
+        return mBinding.root
     }
 }
