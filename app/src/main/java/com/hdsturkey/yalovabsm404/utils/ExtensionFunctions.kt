12b35ecac748c59f8acc7679ab0e356ba27fa5e8
Cengiz TORU <cengiztoru@gmail.com>
2022-03-21 17:13:35 +0300

    Ext Functions prepared to change visibility status of views

---
+++ app/src/main/java/com/hdsturkey/yalovabsm404/utils/ExtensionFunctions.kt
@@ -0,0 +1,15 @@
+package com.hdsturkey.yalovabsm404.utils
+
+import android.view.View
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
