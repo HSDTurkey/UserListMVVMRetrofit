ad2dbe097d7c27f71c56d819629ca4b10346c178
Cengiz TORU <cengiztoru@gmail.com>
2022-03-22 15:26:53 +0300

    Added an ext fun to checking email valid state

--- app/src/main/java/com/hdsturkey/yalovabsm404/utils/ExtensionFunctions.kt
+++ app/src/main/java/com/hdsturkey/yalovabsm404/utils/ExtensionFunctions.kt
@@ -3,6 +3,8 @@ package com.hdsturkey.yalovabsm404.utils
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.text.TextUtils
+import android.util.Patterns
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -30,3 +32,5 @@ fun Fragment.toast(message: String, length: Int = Toast.LENGTH_SHORT) {
 fun Activity.startActivity(targetActivity: Class<*>) {
     startActivity(Intent(this, targetActivity))
 }
+fun String.isValidEmail() =
+    !TextUtils.isEmpty(this) && Patterns.EMAIL_ADDRESS.matcher(this).matches()
