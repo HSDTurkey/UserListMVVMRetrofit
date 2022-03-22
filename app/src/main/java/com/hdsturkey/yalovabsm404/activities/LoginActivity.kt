0a87cc7c7723c01893d631edd2099f3d0632589c
Cengiz TORU <cengiztoru@gmail.com>
2022-03-22 16:08:28 +0300

    Login Screen done. Navigated user to proper activity by checking inputs state.

--- app/src/main/java/com/hdsturkey/yalovabsm404/activities/LoginActivity.kt
+++ app/src/main/java/com/hdsturkey/yalovabsm404/activities/LoginActivity.kt
@@ -1,12 +1,77 @@
 package com.hdsturkey.yalovabsm404.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.hdsturkey.yalovabsm404.R
+import androidx.appcompat.app.AppCompatActivity
+import com.hdsturkey.yalovabsm404.databinding.ActivityLoginBinding
+import com.hdsturkey.yalovabsm404.utils.Constants
+import com.hdsturkey.yalovabsm404.utils.SharedPreferencesHelper
+import com.hdsturkey.yalovabsm404.utils.isValidEmail
+import com.hdsturkey.yalovabsm404.utils.startActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        mBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setListeners()
     }
+
+    private fun setListeners() {
+        mBinding.btnLogin.setOnClickListener {
+            if (isInputsValid()) {
+                if (mBinding.chkRememberMe.isChecked) {
+                    saveUserAndNavigateToNextScreen()
+                } else {
+                    navigateUserToNextScreen()
+                }
+            }
+        }
+    }
+
+    private fun isInputsValid(): Boolean {
+        mBinding.tilUserMail.error = null
+        mBinding.tilPassword.error = null
+
+        val userMail = mBinding.etUserMail.text
+        if (userMail.isNullOrBlank()) {
+            mBinding.tilUserMail.error = "Please enter your email address"
+            return false
+        }
+
+        if (userMail.toString().isValidEmail().not()) {
+            mBinding.tilUserMail.error = "Please enter a valid email address"
+            return false
+        }
+
+        val userPassword = mBinding.etPassword.text
+        if (userPassword.isNullOrBlank()) {
+            mBinding.tilPassword.error = "Please enter your password"
+            return false
+        }
+
+        return true
+    }
+
+    private fun saveUserAndNavigateToNextScreen() {
+        saveUser()
+        navigateUserToNextScreen()
+    }
+
+    private fun saveUser() {
+        val userEmail = mBinding.etUserMail.text.toString()
+        val userPassword = mBinding.etPassword.text.toString()
+        SharedPreferencesHelper.putString(Constants.PREF_USER_EMAIL, userEmail)
+        SharedPreferencesHelper.putString(Constants.PREF_USER_PASSWORD, userPassword)
+    }
+
+    private fun navigateUserToNextScreen() {
+        startActivity(UserActivity::class.java)
+        finish() //so important. It will kill this activity so when clicked on back button in next article this activity  won't appear
+    }
+
 }
--- app/src/main/java/com/hdsturkey/yalovabsm404/utils/Constants.kt
+++ app/src/main/java/com/hdsturkey/yalovabsm404/utils/Constants.kt
@@ -2,4 +2,5 @@ package com.hdsturkey.yalovabsm404.utils
 
 object Constants {
     const val PREF_USER_EMAIL = "user_email"
+    const val PREF_USER_PASSWORD = "user_password"
 }
