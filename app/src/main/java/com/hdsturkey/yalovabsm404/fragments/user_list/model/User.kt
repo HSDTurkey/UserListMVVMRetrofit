026f73b793cdbbdcb92e6fa971fa1027750fb416
Cengiz TORU <cengiztoru@gmail.com>
2022-03-23 13:20:42 +0300

    User entity prepared

--- app/src/main/java/com/hdsturkey/yalovabsm404/fragments/user_list/model/User.kt
+++ app/src/main/java/com/hdsturkey/yalovabsm404/fragments/user_list/model/User.kt
@@ -1,8 +1,18 @@
 package com.hdsturkey.yalovabsm404.fragments.user_list.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+//The following code defines a User data entity. Each instance of User represents a row in a user table in the app's database.
+
+
+@Entity
 data class User(
-    val name: UserName,
-    val gender: String,
-    val phone: String,
-    val picture: UserPicture
+    @ColumnInfo(name = "name") val name: UserName,
+    @ColumnInfo(name = "gender") val gender: String,
+    @ColumnInfo(name = "phone") val phone: String,
+    @ColumnInfo(name = "picture") val picture: UserPicture,
+    @PrimaryKey(autoGenerate = true) val userId: Int = 0 // Each Room entity must define a primary key that uniquely identifies each row in the corresponding database table.
 )
